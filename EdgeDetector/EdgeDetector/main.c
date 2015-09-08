1e5daf5f325d17f734cd351606d5384fd9273b5c
derrick <kang.derrick@gmail.com>
2015-09-08 10:55:15 +0900

    added project

---
+++ EdgeDetector/EdgeDetector/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  EdgeDetector
+//
+//  Created by 강진영 on 9/8/15.
+//  Copyright (c) 2015 sensation. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
