17c5cf2d724b99c0137b0667a04ae0c017bc058e
derrick <kang.derrick@gmail.com>
2015-09-08 13:17:37 +0900

    test about  second array of raw data values

--- EdgeDetector/EdgeDetector/main.c
+++ EdgeDetector/EdgeDetector/main.c
@@ -6,10 +6,50 @@
 //  Copyright (c) 2015 sensation. All rights reserved.
 //
 
+static const int INPUT_NUM=4;
+
 #include <stdio.h>
 
+
+void fileOpen();
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+
+    // todo list
+    // 1. input value module
+    
+//    if( argc != INPUT_NUM ) {
+//        printf("You have to input correctly ! \n");
+//        printf("Input value is 3 \n");
+//        printf("File Name, width, height \n");
+//        exit(1);
+//    } else {
+//        printf("You have to input correctly ! \n");
+//        printf("Input value is 3 \n");
+//        printf("File Name, width, height \n");
+//    }
+////    char fileName[20];
+////    
+////    strcpy(fileName,argv[1]);
+////    
+//
+//  //  printf("file name :  %s /n",fileName);
+  
+    
+    // todo list
+    // 2. Dynamically changing the value.    
+    
+    FILE * fp;
+    char array[256][256];
+    if((fp = fopen("LONDON256.RAW", "rb")) == NULL) {
+        printf( "File  could not be opened\n" );
+    } else {
+        fread(array, 1, 65536, fp);
+    }
+    
+   
+    
+    fclose(fp);
+    
+    
     return 0;
 }
